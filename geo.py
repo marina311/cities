b4a3901365861d08d37f88339baabecfe5881699
marina311 <marina311@mail.ru>
2022-03-21 18:01:47 +0500

    города и страны

--- geo.py
+++ geo.py
@@ -2,12 +2,16 @@ import math
 import requests
 
 
+import requests
+
+
 def get_coordinates(city_name):
     try:
         # url, по которому доступно API Яндекс.Карт
         url = "https://geocode-maps.yandex.ru/1.x/"
         # параметры запроса
         params = {
+            "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
             # город, координаты которого мы ищем
             'geocode': city_name,
             # формат ответа от сервера, в данном случае JSON
@@ -17,15 +21,14 @@ def get_coordinates(city_name):
         response = requests.get(url, params)
         # получаем JSON ответа
         json = response.json()
-        # получаем координаты города (там написаны долгота(longitude),
-        # широта(latitude) через пробел).
-        # Посмотреть подробное описание JSON-ответа можно
-        # в документации по адресу
-        # https://tech.yandex.ru/maps/geocoder/
+        # получаем координаты города
+        # (там написаны долгота(longitude), широта(latitude) через пробел)
+        # посмотреть подробное описание JSON-ответа можно
+        # в документации по адресу https://tech.yandex.ru/maps/geocoder/
         coordinates_str = json['response']['GeoObjectCollection'][
             'featureMember'][0]['GeoObject']['Point']['pos']
-        # Превращаем string в список, так как точка -
-        # это пара двух чисел - координат
+        # Превращаем string в список, так как
+        # точка - это пара двух чисел - координат
         long, lat = map(float, coordinates_str.split())
         # Вернем ответ
         return long, lat
@@ -37,16 +40,20 @@ def get_country(city_name):
     try:
         url = "https://geocode-maps.yandex.ru/1.x/"
         params = {
+            "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
             'geocode': city_name,
             'format': 'json'
         }
         data = requests.get(url, params).json()
         # все отличие тут, мы получаем имя страны
-        return data['response']['GeoObjectCollection']['featureMember'][0][
-            'GeoObject']['metaDataProperty']['GeocoderMetaData'][
-            'AddressDetails']['Country']['CountryName']
+        return data['response']['GeoObjectCollection'][
+            'featureMember'][0]['GeoObject']['metaDataProperty'][
+            'GeocoderMetaData']['AddressDetails']['Country']['CountryName']
     except Exception as e:
         return e
+
+
+import math
 
 
 def get_distance(p1, p2):
@@ -61,8 +68,7 @@ def get_distance(p1, p2):
     d_lon = lon2 - lon1
     d_lat = lat2 - lat1
 
-    a = math.sin(d_lat / 2) ** 2 + math.cos(lat1) * \
-        math.cos(lat2) * math.sin(d_lon / 2) ** 2
+    a = math.sin(d_lat / 2) ** 2 + math.cos(lat1) * math.cos(lat2) * math.sin(d_lon / 2) ** 2
     c = 2 * math.atan2(a ** 0.5, (1 - a) ** 0.5)
 
     distance = radius * c
